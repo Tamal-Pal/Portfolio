0064870d09a5e22e68a3970097bfc4b3a2a0b537
Tamal-Pal <80611886+Tamal-Pal@users.noreply.github.com>
2023-07-08 20:24:30 +0530

    animated update

--- script.js
+++ script.js
@@ -26,10 +26,26 @@ window.onscroll = () =>{
                 links.classList.remove('active');
                 document.querySelector('.header .navbar a[href*='+ id +']').classList.add('active')
             });
-        };
+
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
 
     });
+
+    // animation on footer
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 
 
+// document.getElementById("myForm").addEventListener("submit", function(event) {
+//     event.preventDefault(); // Prevent form submission
+
+//     // Redirect to thanks.html
+//     window.location.href = "thanks.html";
+// });
 
